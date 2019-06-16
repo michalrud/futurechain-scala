d17b4edd8ecf66251640b6f2a139aea66a169dca
Michał Rudowicz <michal.rudowicz@fl9.eu>
2019-06-16 18:48:50 +0200

    Documentation

---
+++ README.md
@@ -0,0 +1,27 @@
+# FutureChain
+
+[![Build Status](https://travis-ci.org/michalrud/futurechain-scala.svg?branch=master)](https://travis-ci.org/michalrud/futurechain-scala)
+
+Allows you to chain your futures like promises in JavaScript.
+
+My first Scala project, so by default assume that it's shitty, unreliable and pretty much a carbon copy of how I would
+do this in other languages I know.
+
+## Assumptions
+
+1. There should be no need to do callback pyramids,
+2. It should be pleasant to use,
+3. Any error should break the chain by default,
+4. Types should be checked to help catch issues early
+
+## Usage example
+
+```scala
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+FutureChain.futureChain(_ => Future{1})
+    .andThen((v: Int) => Future(v.toString))
+    .andThenSync((v: String) => s"Value is $v")
+    .run()
+```
--- src/main/scala/ChainElement.scala
+++ src/main/scala/ChainElement.scala
@@ -3,19 +3,43 @@ import scala.concurrent.Future
 trait ChainElement[T] {
   private var alreadyStarted = false
 
+  /**
+    * Runs the future chain.
+    *
+    * @return A Future returned by the last chain element.
+    */
   final def run(): Future[T] = {
     assert(!alreadyStarted, "Attempted to run an already started FutureChain")
     alreadyStarted = true
     _run()
   }
 
+  /**
+    * Adds a new element to the future chain.
+    *
+    * @param cbk Element to be added to the chain. Should return a Future object.
+    * @tparam U Type returned by the object to be added, as enclosed in Future.
+    * @return Last element in the future chain.
+    */
   def andThen[U](cbk: T => Future[U]): ChainElement[U] = {
     new AsynchronousChainElement[T, U, this.type](this, cbk)
   }
 
+  /**
+    * Adds a new synchronous element to the future chain.
+    *
+    * @param cbk Element to be added to the chain. Can return anything, but next element in the chain won't wait for it.
+    * @tparam U Type returned by the element to be added.
+    * @return Last element in the future chain.
+    */
   def andThenSync[U](cbk: T => U): ChainElement[U] = {
     new SynchronousChainElement[T, U, this.type](this, cbk)
   }
 
+  /**
+    * To be reimplemented by the chain element classes.
+    *
+    * @return Result of the chain element execution in the Future.
+    */
   protected def _run(): Future[T]
 }
